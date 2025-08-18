037e6563f193fcfc50c47041c89412e79758e3d5
Frederic Mercier <f.mercier@fr.ibm.com>
2025-08-18 17:57:23 +0200

    add a description to Miniloan Service

--- samples/deploy_demo_materials.sh
+++ samples/deploy_demo_materials.sh
@@ -57,29 +57,44 @@ import_and_deploy_decision_service() {
 
   local DS_NAME_URLENCODED="${DS_NAME// /%20}"
 
-  echo "🔽 Downloading archive for '${DS_NAME}'..."
+#  echo "🔽 Downloading archive for '${DS_NAME}'..."
 #  curl -sL -o "${DS_FILENAME}" "https://github.com/DecisionsDev/odm-for-dev-getting-started/blob/master/${DS_FILENAME_URLENCODED}?raw=1"
 
   echo "📤 Importing '${DS_NAME}' into Decision Center..."
-  curl -sk -X POST "${AUTH_CREDENTIALS[@]}" \
+  local IMPORT_RESULT=$(curl -sk -X POST "${AUTH_CREDENTIALS[@]}" \
     -H "accept: application/json" \
     -H "Content-Type: multipart/form-data" \
     --form "file=@${DS_FILENAME};type=application/zip" \
-    "${DC_API_URL}/v1/decisionservices/import"
-
-  echo "🔍 Retrieving service ID..."
-  local GET_DECISIONSERVICE_RESULT=$(curl -sk -X GET "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
-    "${DC_API_URL}/v1/decisionservices?q=name%3A${DS_NAME_URLENCODED}")
-  local DECISIONSERVICEID=$(echo "${GET_DECISIONSERVICE_RESULT}" | jq -r '.elements[0].id')
+    "${DC_API_URL}/v1/decisionservices/import")
+  local DECISIONSERVICEID=$(echo ${IMPORT_RESULT} | jq -r '.decisionService.id')
+  if [ "${DECISIONSERVICEID}" = "null" ]; then
+    # the decision service is problably already imported.
+    # Get its ID
+    echo "🔍 Retrieving service ID..."
+    local GET_DECISIONSERVICE_RESULT=$(curl -sk -X GET "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
+      "${DC_API_URL}/v1/decisionservices?q=name%3A${DS_NAME_URLENCODED}")
+    DECISIONSERVICEID=$(echo "${GET_DECISIONSERVICE_RESULT}" | jq -r '.elements[0].id')
+    if [ "${DECISIONSERVICEID}" = "null" ]; then
+      echo ${IMPORT_RESULT} | jq
+    fi
+  fi
 
   echo "🔍 Retrieving deployment config ID..."
-  local GET_DEPLOYMENT_RESULT=$(curl -sk -X GET "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
+  local DEPLOYMENT_CONFIG_RESULT=$(curl -sk -X GET "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
     "${DC_API_URL}/v1/decisionservices/${DECISIONSERVICEID}/deployments?q=name%3A${DEPLOYMENT_NAME_URLENCODED}")
-  local DEPLOYMENTCONFIGURATIONID=$(echo "${GET_DEPLOYMENT_RESULT}" | jq -r '.elements[0].id')
+  local DEPLOYMENTCONFIGURATIONID=$(echo "${DEPLOYMENT_CONFIG_RESULT}" | jq -r '.elements[0].id')
+  if [ "${DEPLOYMENTCONFIGURATIONID}" = "null" ]; then
+    echo ${DEPLOYMENT_CONFIG_RESULT} | jq
+  fi
 
   echo "🚀 Deploying '${DS_NAME}'..."
-  curl -sk -X POST "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
-    "${DC_API_URL}/v1/deployments/${DEPLOYMENTCONFIGURATIONID}/deploy" | jq
+  local DEPLOYMENT_RESULT=$(curl -sk -X POST "${AUTH_CREDENTIALS[@]}" -H "accept: application/json" \
+    "${DC_API_URL}/v1/deployments/${DEPLOYMENTCONFIGURATIONID}/deploy")
+  DEPLOYMENT_ID=$(echo ${DEPLOYMENT_RESULT} | jq -r .id)
+  if [ "${DEPLOYMENT_ID}" = "null" ]; then
+    echo ${DEPLOYMENT_RESULT} | jq
+  fi
+
 }
 
 # ----------------------------------
@@ -88,4 +103,5 @@ import_and_deploy_decision_service() {
 
 # You can repeat the line below for other decision services
 import_and_deploy_decision_service "Beauty Advisory Service" "deployment" "Beauty_Advisory_Service.zip"
-import_and_deploy_decision_service "Vacation Service" "deployment" "Vacation_Service.zip" 
+import_and_deploy_decision_service "Vacation Service"        "deployment" "Vacation_Service.zip" 
+import_and_deploy_decision_service "Agent Miniloan Service"  "Miniloan"   "Agent_Miniloan_Service.zip"
